75acea3af9b119fc31dea3244df2bd51c20b5bd6
dorogov <dorogov.anton@gmail.com>
2018-03-19 16:27:55 +0300

    написал функции

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,25 @@
-
+import re
 
 def load_data(filepath):
-    pass
+    with open('filepath', 'r') as f:
+        d = f.read() #читаем файл целиком
+    d = re.split(r'[\d\W+]', d) # разделяем текст рек выражением - разделители все что не буквы
+    d = [token.lower() for token in d]# приводим все к нижнему регистру
+    s = {}
+    for elem in d:# создаем словарь из слов в списке - ключ и количествоих в тексте - значение
+        s[elem] = s.get(elem, 0) + 1
+    t = []
+    for key in sorted(s):# создаем сортированный список списков - слово,количество попаданий
+        t.append([key, s[key]])
+    return t
 
 
 def get_most_frequent_words(text):
-    pass
+    10_words = sorted(text, key=lambda x: x[1], reverse=True)
+    del 10_words[0]
+    return 10_words[:10]
 
 
 if __name__ == '__main__':
-    pass
+    g = get_most_frequent_words(load_data())
+    print(g)
