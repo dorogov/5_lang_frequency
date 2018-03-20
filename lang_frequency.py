6cdc2f0389d1ad098bba91668ff7dc723121c3d0
dorogov <36375571+dorogov@users.noreply.github.com>
2018-03-20 12:44:46 +0300

    Update lang_frequency.py
    переписал все)

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,23 +1,13 @@
 import re
+import collections as cll
 
 def load_data(filepath):
-    with open('filepath', 'r') as f:
-        d = f.read() #читаем файл целиком
-    d = re.split(r'[\d\W+]', d) # разделяем текст рек выражением - разделители все что не буквы
-    d = [token.lower() for token in d]# приводим все к нижнему регистру
-    s = {}
-    for elem in d:# создаем словарь из слов в списке - ключ и количествоих в тексте - значение
-        s[elem] = s.get(elem, 0) + 1
-    t = []
-    for key in sorted(s):# создаем сортированный список списков - слово,количество попаданий
-        t.append([key, s[key]])
-    return t
+    data = re.findall('\w+', open(filepath).read().lower())
+    return data
 
 
 def get_most_frequent_words(text):
-    ten_words = sorted(text, key=lambda x: x[1], reverse=True)
-    del ten_words[0]
-    return ten_words[:10]
+    return cll.Counter(text).most_common(10)
 
 
 if __name__ == '__main__':
