a7324280c37ea3b65e98d487c5b99476d8223d13
dorogov <36375571+dorogov@users.noreply.github.com>
2018-03-21 09:03:54 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,5 +1,5 @@
 import re
-import collections as cll
+from collections import Counter
 
 def load_data(filepath):
     list_of_words = re.findall('\w+', open(filepath).read().lower())
@@ -7,10 +7,10 @@ def load_data(filepath):
 
 
 def get_most_frequent_words(text):
-    number_words_max_frequency = 10
-    return cll.Counter(text).most_common(number_words_max_frequency)
+    number_words_with_max_frequency = 10
+    return Counter(text).most_common(number_words_with_max_frequency)
 
 
 if __name__ == '__main__':
-    word_frequency = get_most_frequent_words(load_data())
+    word_frequency = get_most_frequent_words(load_data(filepath))
     print(word_frequency)
