f0ce06b7c223a67d57c1241c80f105d1b95a066e
dorogov <36375571+dorogov@users.noreply.github.com>
2018-03-21 22:17:39 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,16 +1,23 @@
+import sys
 import re
 from collections import Counter
 
+filepath = sys.argv[1]
+
+
 def load_data(filepath):
-    list_of_words = re.findall('\w+', open(filepath).read().lower())
-    return list_of_words
+    with open(filepath) as file:
+        text = file.read().lower()
+    return text
 
 
 def get_most_frequent_words(text):
-    number_words_with_max_frequency = 10
-    return Counter(text).most_common(number_words_with_max_frequency)
+    list_of_words = re.findall('\w+', text)
+    number_words_show = 10
+    return Counter(list_of_words).most_common(number_words_show)
 
 
 if __name__ == '__main__':
     word_frequency = get_most_frequent_words(load_data(filepath))
-    print(word_frequency)
+    for _ in word_frequency:
+        print('слово - {} - встречается - {} - раз'.format(_[0], _[1]))
