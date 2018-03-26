55655112beaa2d93a7c87482f37b59c303b96622
dorogov <36375571+dorogov@users.noreply.github.com>
2018-03-26 08:58:04 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,17 @@
 import sys
+import os
 import re
 from collections import Counter
+import argparse
 
 
 def load_data(filepath):
+    if not os.path.exists(filepath):
+        return None
     with open(filepath) as file:
         text = file.read()
     return text
+
 
 
 def get_most_frequent_words(text):
@@ -16,7 +21,11 @@ def get_most_frequent_words(text):
 
 
 if __name__ == '__main__':
-    filepath = sys.argv[1]
-    words = get_most_frequent_words(load_data(filepath))
-    for token in words:
-        print('слово - {} - вхождений - {}'.format(token[0], token[1]))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', required=True, metavar='ФАЙЛ',
+                        help='Путь до текстового файла.')
+    namespace = parser.parse_args()
+    data = load_data(namespace.file)
+    ten_words = get_most_frequent_words(data)
+    for key, item in ten_words:
+        print('слово - {} - вхождений - {}'.format(key, item))
