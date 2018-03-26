e1121ea4170fdcfccf5a6978aa1e30efd3140989
dorogov <36375571+dorogov@users.noreply.github.com>
2018-03-26 23:12:02 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -21,7 +21,9 @@ def get_most_frequent_words(text):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', required=True, metavar='ФАЙЛ', \
+    parser.add_argument('-f', '--file', 
+                        required=True, 
+                        metavar='ФАЙЛ',
                         help='Путь до текстового файла.')
     args = parser.parse_args()
     text = load_data(args.file)
